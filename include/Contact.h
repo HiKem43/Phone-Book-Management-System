38297196432a5326ff28f407b51832daa163acea
Uyên Mỹ <uyenntm757784@ut.edu.vn>
2026-09-10 00:21:47 +0700

    Add files via upload

---
+++ include/Contact.h
@@ -0,0 +1,38 @@
+#ifndef CONTACT_H
+#define CONTACT_H
+#include <string>
+using namespace std;
+class Contact
+{
+private:
+    int contact_id;
+    int account_id;
+    int group_id;
+    string name;
+    string phone;
+    string email;
+    string address;
+    bool isFavorite;
+public:
+    Contact();
+    Contact(int contact_id, int account_id, int group_id,
+        string name, string phone, string email,
+        string address, bool isFavorite);
+    int getContactId() const;
+    int getAccountId() const;
+    int getGroupId() const;
+    string getName() const;
+    string getPhone() const;
+    string getEmail() const;
+    string getAddress() const;
+    bool getIsFavorite() const;
+    void setContactId(int contact_id);
+    void setAccountId(int account_id);
+    void setGroupId(int group_id);
+    void setName(string name);
+    void setPhone(string phone);
+    void setEmail(string email);
+    void setAddress(string address);
+    void setIsFavorite(bool isFavorite);
+};
+#endif
---
+++ include/Group.h
@@ -0,0 +1,25 @@
+#ifndef GROUP_H
+#define GROUP_H
+#include <string>
+using namespace std;
+class Group
+{
+private:
+    int group_id;
+    int account_id;
+    string groupName;
+    string description;
+public:
+    Group();
+    Group(int group_id, int account_id,
+        string groupName, string description);
+    int getGroupId() const;
+    int getAccountId() const;
+    string getGroupName() const;
+    string getDescription() const;
+    void setGroupId(int group_id);
+    void setAccountId(int account_id);
+    void setGroupName(string groupName);
+    void setDescription(string description);
+};
+#endif
